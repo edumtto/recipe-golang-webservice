7eddcff1ba8cd909985d24e56395724a5e8ca632
Eduardo Motta de Oliveira <edumottaoliveira@gmail.com>
2021-02-16 18:44:46 -0300

    Marshal json using snake_case properties

--- src/domain/entity.go
+++ src/domain/entity.go
@@ -9,50 +9,50 @@ const (
 
 // RecipeAuthor struct
 type RecipeAuthor struct {
-	ID   int
-	Name string
+	ID   int    `json:"id"`
+	Name string `json:"name"`
 }
 
 // RecipeCategory struct
 type RecipeCategory struct {
-	ID   int
-	Name string
+	ID   int    `json:"id"`
+	Name string `json:"name"`
 }
 
 // RecipeDifficulty struct
 type RecipeDifficulty struct {
-	ID   int
-	Name string
+	ID   int    `json:"id"`
+	Name string `json:"name"`
 }
 
 // Recipe struct
 type Recipe struct {
-	ID              int
-	Title           string
-	Description     string
-	Author          RecipeAuthor
-	Category        RecipeCategory
-	Difficulty      RecipeDifficulty
-	Rating          int
-	PreparationTime int
-	Serving         int
-	Ingredients     []string
-	Steps           []string
-	PublishedDate   string
-	AccessCount     int
-	ImageURL        string
+	ID              int              `json:"id"`
+	Title           string           `json:"title"`
+	Description     string           `json:"description"`
+	Author          RecipeAuthor     `json:"author"`
+	Category        RecipeCategory   `json:"category"`
+	Difficulty      RecipeDifficulty `json:"difficulty"`
+	Rating          int              `json:"rating"`
+	PreparationTime int              `json:"preparation_time"`
+	Serving         int              `json:"serving"`
+	Ingredients     []string         `json:"ingredients"`
+	Steps           []string         `json:"steps"`
+	PublishedDate   string           `json:"published_date"`
+	AccessCount     int              `json:"access_count"`
+	ImageURL        string           `json:"image_url"`
 }
 
 // RecipePreview struct
 type RecipePreview struct {
-	ID          int
-	Title       string
-	Description string
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
 }
 
 // RecipeForm struct
 type RecipeForm struct {
-	Recipe       Recipe
-	Categories   []RecipeCategory
-	Difficulties []RecipeDifficulty
+	Recipe       Recipe             `json:"recipe"`
+	Categories   []RecipeCategory   `json:"categories"`
+	Difficulties []RecipeDifficulty `json:"difficulties"`
 }
