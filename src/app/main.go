7521cacfbce2cdb12d718d2c550ecbf925d1349f
Eduardo Motta de Oliveira <edumottaoliveira@gmail.com>
2021-04-13 22:04:31 -0300

    Back with the single entry point main.go

--- src/app/main.go
+++ src/app/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"regexp"
 
 	"github.com/Edu15/recipe-golang-webservice/src/domain"
@@ -25,17 +26,66 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 	}
 }
 
+func recipesHandler(w http.ResponseWriter, r *http.Request) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		fmt.Println("Invalid path!")
+		http.NotFound(w, r)
+		return
+	}
+
+	recipeID := m[2]
+
+	switch r.Method {
+	case http.MethodGet:
+		if recipeID == "form" {
+			recipeService.New(w, r, "")
+		} else if recipeID != "" {
+			recipeService.View(w, r, recipeID)
+		} else {
+			recipeService.List(w, r, "")
+		}
+
+	case http.MethodPost:
+		recipeService.Create(w, r, "")
+
+	case http.MethodPut:
+		recipeService.Update(w, r, recipeID)
+
+	case http.MethodDelete:
+		recipeService.Delete(w, r, recipeID)
+
+	default:
+		http.NotFound(w, r)
+	}
+}
+
 /*
+
+POST /recipes to add a new recipe
+GET /recipes to fetch all existing recipes
+GET /recipes/form to fech a form to create a new recipe
+GET /recipes/{recipeId} to fetch a single recipe using its ID
+PUT /recipes/{recipeId} to update an existing recipe
+DELETE /recipes/{itemId} to delete a recipe
+
+// Only for HTML forms:
 GET /new to fech a form to create a new recipe
 GET /create to fech a form to create a new recipe
 GET /edit to fech a form to create a new recipe
 GET /update/{recipeId} to update an existing recipe
 GET /delete/{recipeId} to delete a recipe
+
 */
 
 func main() {
-	recipeService = recipe.NewService(domain.HTML)
+	responseFormat := domain.JSON
+	if len(os.Args) > 1 && os.Args[1] == "html" {
+		responseFormat = domain.HTML
+	}
+	recipeService = recipe.NewService(responseFormat)
 
+	http.HandleFunc("/recipes/", recipesHandler)
 	http.HandleFunc("/new/", makeHandler(recipeService.New))
 	http.HandleFunc("/create/", makeHandler(recipeService.Create))
 	http.HandleFunc("/edit/", makeHandler(recipeService.Edit))
